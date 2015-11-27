e34e2366438d48979e4fd170c91fe11188a32a5e
nojaf <florian.verdonck@outlook.com>
2015-11-27 21:34:10 +0100

    Improved test readability

--- tests/actions/people.spec.js
+++ tests/actions/people.spec.js
@@ -1,22 +1,28 @@
 import actions from "actions/people";
 import {ADD_PERSON, DELETE_PERSON, GENERATE} from "constants";
 
-describe("invoking actions", () => {
-	it("should return a ADD_PERSON with value", () => {
-		const action = actions.addPerson("Hank");
-		expect(action.type).to.equal(ADD_PERSON);
-		expect(action.payload.value).to.equal("Hank");
-	})
-
-	it("should return DELETE_PERSON with index", () => {
-		const action = actions.removePerson(0);
-		expect(action.type).to.equal(DELETE_PERSON);
-		expect(action.payload.index).to.equal(0);
+describe("actions", () => {
+	describe("invoking action addPerson", () => {
+		it("should return a ADD_PERSON with value", () => {
+			const action = actions.addPerson("Hank");
+			expect(action.type).to.equal(ADD_PERSON);
+			expect(action.payload.value).to.equal("Hank");
+		});
 	});
 
-	it("should return GENERATE without payload", () => {
-		const action = actions.generate();
-		expect(action.type).to.equal(GENERATE);
-		expect(action.payload).to.undefined;
+	describe("invoking action removePerson", () => {
+		it("should return DELETE_PERSON with index", () => {
+			const action = actions.removePerson(0);
+			expect(action.type).to.equal(DELETE_PERSON);
+			expect(action.payload.index).to.equal(0);
+		});
+	});
+
+	describe("invoking action generate", () => {
+		it("should return GENERATE without payload", () => {
+			const action = actions.generate();
+			expect(action.type).to.equal(GENERATE);
+			expect(action.payload).to.undefined;
+		});
 	});
 });
--- tests/reducers/people.spec.js
+++ tests/reducers/people.spec.js
@@ -2,74 +2,83 @@ import {default as peopleReducer} from "reducers/people";
 import {ADD_PERSON, DELETE_PERSON, GENERATE} from "constants";
 
 describe("people reducers", () => {
-	it("should return the previous state", () => {
-		const state = {};
-		Object.freeze(state);
 
-		const newState = peopleReducer(state, {type:"SometingElse"});
-		assert.equal(state, newState);
-	});
+	describe("passed an unkown type", () => {
+		it("should return the previous state", () => {
+			const state = {};
+			Object.freeze(state);
 
-	it("should add a new person", () => {
-		const state = {};
-		Object.freeze(state);
+			const newState = peopleReducer(state, {type:"SometingElse"});
+			assert.equal(state, newState);
+		});
+	})
 
-		const newState = peopleReducer(state, {type:ADD_PERSON, payload:{"value":"Jimmy"}});
-		assert.equal("Jimmy",newState.persons[0]);
-	});
+	describe("passed an ADD_PERSON type", () => {
+		it("should add a new person", () => {
+			const state = {};
+			Object.freeze(state);
 
-	it("should clear the results when adding a person", () => {
-		const state = {persons:["Tim","Peter"], results:new Map()};
-		Object.freeze(state);
+			const newState = peopleReducer(state, {type:ADD_PERSON, payload:{"value":"Jimmy"}});
+			assert.equal("Jimmy",newState.persons[0]);
+		});
 
-		const newState = peopleReducer(state, {type:ADD_PERSON, payload:{"value":"Gilles"}});
-		assert.isNull(newState.results);		
-	});
+		it("should clear the results when adding a person", () => {
+			const state = {persons:["Tim","Peter"], results:new Map()};
+			Object.freeze(state);
 
-	it("should remove a person", () => {
-		const state = {persons:["Tim", "Bob", "James"]};
-		Object.freeze(state);
-
-		const newState = peopleReducer(state, {type:DELETE_PERSON, payload:{"index":2}});
-		assert.equal("Bob", newState.persons[newState.persons.length - 1]);
-	});
-
-	it("should clear the results when removing a person", () => {
-		const state = {persons:["Tim", "Dave", "Mike"], results:new Map()};
-		Object.freeze(state);
-
-		const newState = peopleReducer(state, {type:DELETE_PERSON, payload:{"index":0}});
-		assert.isNull(newState.results);
-	});
-
-	it("should generate a resultset", () => {
-		const state = {persons:["Dave", "Hanna"]};
-		Object.freeze(state);
-
-		const newState = peopleReducer(state, {type:GENERATE});
-		assert.isNotNull(newState.results);
-	});
-
-	it("should generate a matching circle", () => {
-		const state = {persons:["Dave", "Hanna","Henry"]};
-		Object.freeze(state);
-
-		const newState = peopleReducer(state, {type:GENERATE});
-		newState.results.forEach((key,value) => {
-			assert.notEqual(key, newState.results[key]);
+			const newState = peopleReducer(state, {type:ADD_PERSON, payload:{"value":"Gilles"}});
+			assert.isNull(newState.results);		
 		});
 	});
 
-	function onlyUnique(value, index, self) { 
-	    return self.indexOf(value) === index;
-	}
+	describe("passed a DELETE_PERSON type", () => {
+		it("should remove a person", () => {
+			const state = {persons:["Tim", "Bob", "James"]};
+			Object.freeze(state);
 
-	it("should not contain duplicate values", () => {
-		const state = {persons:["James","Harvey","Bruce","Alfred"]};
-		Object.freeze(state);
+			const newState = peopleReducer(state, {type:DELETE_PERSON, payload:{"index":2}});
+			assert.equal("Bob", newState.persons[newState.persons.length - 1]);
+		});
 
-		const newState = peopleReducer(state, {type:GENERATE});
-		const uniqueValues = Array.from(newState.results.values()).filter(onlyUnique);
-		assert.equal(newState.results.size, uniqueValues.length);
+		it("should clear the results when removing a person", () => {
+			const state = {persons:["Tim", "Dave", "Mike"], results:new Map()};
+			Object.freeze(state);
+
+			const newState = peopleReducer(state, {type:DELETE_PERSON, payload:{"index":0}});
+			assert.isNull(newState.results);
+		});
+	});
+
+	describe("passed a GENERATE type", () => {
+		it("should generate a resultset", () => {
+			const state = {persons:["Dave", "Hanna"]};
+			Object.freeze(state);
+
+			const newState = peopleReducer(state, {type:GENERATE});
+			assert.isNotNull(newState.results);
+		});
+
+		it("should generate a matching circle", () => {
+			const state = {persons:["Dave", "Hanna","Henry"]};
+			Object.freeze(state);
+
+			const newState = peopleReducer(state, {type:GENERATE});
+			newState.results.forEach((key,value) => {
+				assert.notEqual(key, newState.results[key]);
+			});
+		});
+
+		function onlyUnique(value, index, self) { 
+		    return self.indexOf(value) === index;
+		}
+
+		it("should not contain duplicate values", () => {
+			const state = {persons:["James","Harvey","Bruce","Alfred"]};
+			Object.freeze(state);
+
+			const newState = peopleReducer(state, {type:GENERATE});
+			const uniqueValues = Array.from(newState.results.values()).filter(onlyUnique);
+			assert.equal(newState.results.size, uniqueValues.length);
+		});
 	});
 });
